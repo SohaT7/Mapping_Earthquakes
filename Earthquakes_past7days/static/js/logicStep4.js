d99a49d6a841a0b18cda8079059feb05a2fb1d2e
SohaT7 <st.soha@gmail.com>
2022-06-02 15:44:29 -0400

    Adding logicStep4 file

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -35,7 +35,7 @@ let map = L.map('mapid', {
   center: [39.5, -98.5],
   zoom: 3,
   layers: [streets]
-})
+});
 
 // Then we add a control to the map that will allow the user to change
 // which layers are visible.
@@ -45,7 +45,7 @@ L.control.layers(baseMaps, overlays).addTo(map);
 //L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // This function returns the style data for each of the earthquakes we plot on
   // the map. We pass the magnitude of the earthquake into a function
   // to calculate the radius.
@@ -95,7 +95,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
     // We turn each feature into a circleMarker on the map.
 
-    pointToLayer: function (feature, latlng) {
+    pointToLayer: function(feature, latlng) {
       console.log(data);
       return L.circleMarker(latlng);
     },
@@ -104,8 +104,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     style: styleInfo,
       // We create a popup for each circleMarker to display the magnitude and
       //  location of the earthquake after the marker has been created and styled.
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + layer.feature.properties.mag + "<br>Location: " + layer.feature.properties.place);
       }
   }).addTo(earthquakes);
 
